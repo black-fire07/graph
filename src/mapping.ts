cfefcbd25a38d484eb341d6cd64b4e3f04bb1883
black-fire07 <deepeshrawat77@gmail.com>
2022-02-01 14:24:11 +0530

    first

--- src/mapping.ts
+++ src/mapping.ts
@@ -17,7 +17,8 @@ export function handleGet(event: Get): void {
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  
+  entity.count = entity.count.plus(BigInt.fromI32(1))
 
   // Entity fields can be set based on event parameters
   entity.add = event.params.add
